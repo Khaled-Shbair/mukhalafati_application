e4e65ad4af13dd1735794b049afc3bffab49bd0c
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-07 16:59:53 +0300

    Create routes file to control all routes in application

---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,23 @@
+import '/config/all_imports.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text(ManagerStrings.noRouteFound),
+        ),
+        body: const Center(
+          child: Text(ManagerStrings.noRouteFound),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,3 @@
+class Routes {
+  static const String splashScreen='Splash_Screen';
+}
