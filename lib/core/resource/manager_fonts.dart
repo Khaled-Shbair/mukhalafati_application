c80d48e66af432f6b2fa757f3b3f0f039a87ffe1
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-07 17:04:31 +0300

    Create manager_fonts.dart to contain all proprieties of text in application

---
+++ lib/core/resource/manager_fonts.dart
@@ -0,0 +1,21 @@
+import 'package:mukhalafati_application/core/util/size_uitl.dart';
+
+import '/config/all_imports.dart';
+
+class ManagerFontsSizes {
+  static double f13 = SizeUitl.setFontSize(13);
+  static double f14 = SizeUitl.setFontSize(14);
+  static double f15 = SizeUitl.setFontSize(15);
+  static double f18 = SizeUitl.setFontSize(18);
+  static double f22 = SizeUitl.setFontSize(22);
+}
+
+class ManagerFontWeight {
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class ManagerFontFamily {
+  static const String cairo = 'Cairo';
+}
