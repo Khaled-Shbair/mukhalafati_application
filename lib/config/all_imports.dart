2f0b10ed12776a3dad7b4ac1ed3796e57ef57840
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-07 17:00:53 +0300

    Create all_imports.dart to contain all imports of application

---
+++ lib/config/all_imports.dart
@@ -0,0 +1,10 @@
+export 'package:get/get.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+
+export '/routes/routes.dart';
+export '/routes/route_generator.dart';
+
+export '/config/constants.dart';
+
+export '/core/resource/manager_strings.dart';
