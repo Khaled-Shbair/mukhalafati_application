b12a5b80467f703e27c196f125023952f1a9b932
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-07 17:03:17 +0300

    Create manager_colors.dart to contain all colors of application

---
+++ lib/core/resource/manager_colors.dart
@@ -0,0 +1,12 @@
+import '/config/all_imports.dart';
+
+class ManagerColors {
+  static const Color primaryColor = Color(0XFFCDAB35);
+  static const Color americanGold = Color(0XFFCEAD3B);
+  static const Color philippineGray = Color(0XFF978D8D);
+  static const Color quartz = Color.fromRGBO(78, 74, 74, 25);
+  static const Color gunmetal = Color.fromRGBO(38, 50, 56, 80);
+  static const Color blackOlive = Color(0XFF404040);
+  static const Color white = Color(0XFFFFFFFF);
+  static const Color black = Color(0XFF000000);
+}
