2b5dedca0b0d13f9c783ff880f0c3672545c3458
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-07 17:02:56 +0300

    Create manager_assets.dart to contain all paths of images

---
+++ lib/core/resource/manager_assets.dart
@@ -0,0 +1,7 @@
+const String _imagePath = 'assets/images';
+
+class ManagerAssets {
+  static const String logo = '$_imagePath/logo.svg';
+  static const String backgroundSplashScreen =
+      '$_imagePath/splash/background_splash_screen.svg';
+}
