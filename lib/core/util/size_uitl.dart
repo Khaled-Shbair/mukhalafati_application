fb079dbc53fa5f9d4fa937e59245ccde1dadba42
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-07 17:05:15 +0300

    Create size_uitl.dart to control all sizes in application

---
+++ lib/core/util/size_uitl.dart
@@ -0,0 +1,19 @@
+import '/config/all_imports.dart';
+
+class SizeUitl {
+  static double setWidth(double width) {
+    return width.w;
+  }
+
+  static double setHeight(double height) {
+    return height.h;
+  }
+
+  static double setRadius(double radius) {
+    return radius.r;
+  }
+
+  static double setFontSize(double fontSize) {
+    return fontSize.sp;
+  }
+}
