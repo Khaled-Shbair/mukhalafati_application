7eeaf5190315a13527ae9733b2aed6574487836a
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-08 13:22:42 +0300

    add new imports

--- lib/config/all_imports.dart
+++ lib/config/all_imports.dart
@@ -1,10 +1,19 @@
 export 'package:get/get.dart';
+export 'package:get_it/get_it.dart';
 export 'package:flutter/material.dart';
+export 'package:flutter_svg/flutter_svg.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 
 export '/routes/routes.dart';
 export '/routes/route_generator.dart';
 
 export '/config/constants.dart';
-
+export '/core/util/size_uitl.dart';
+export '/core/resource/manager_sizes.dart';
+export '/config/dependency_injection.dart';
+export '/core/resource/manager_assets.dart';
+export '/core/resource/manager_colors.dart';
 export '/core/resource/manager_strings.dart';
+
+export '/features/splash/presentation/view/screens/splash_screen.dart';
+export '/features/splash/presentation/controller/splash_controller.dart';
