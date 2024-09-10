aa73c1fe598e9dc1e53e0c45f7c331604fcd365f
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-10 16:24:33 +0300

    Add new Strings

--- lib/core/resource/manager_strings.dart
+++ lib/core/resource/manager_strings.dart
@@ -3,11 +3,24 @@ import '/config/all_imports.dart';
 class ManagerStrings {
   static String noRouteFound = 'noRouteFound'.tr;
   static String skip = 'skip'.tr;
-
   static String policeMan = 'police_man'.tr;
   static String subTitleOnBoardingOne = 'sub_title_on_boarding_one'.tr;
-  static String driver = 'driver'.tr;
+  static String theDriver = 'the_driver'.tr;
   static String subTitleOnBoardingTwo = 'sub_title_on_boarding_two'.tr;
-  static String violations = 'Violations'.tr;
+  static String violations = 'violations'.tr;
   static String subTitleOnBoardingThree = 'sub_title_on_boarding_three'.tr;
+  static String welcome = 'welcome'.tr;
+  static String subTitleLogin = 'sub_title_login'.tr;
+  static String login = 'login'.tr;
+  static String createAccount = 'create_account'.tr;
+  static String welcomeBackToOurApp = 'welcome_back_to_our_app'.tr;
+  static String police = 'police'.tr;
+  static String driver = 'driver'.tr;
+  static String jobNumber = 'job_number'.tr;
+  static String rememberMe = 'remember_me'.tr;
+  static String forgotYourPassword = 'forgot_your_password'.tr;
+  static String doNotHaveAnAccount = 'do_not_have_an_account'.tr;
+  static String licenseNumber = 'license_number'.tr;
+  static String subscribe = 'subscribe'.tr;
+  static String password = 'password'.tr;
 }
