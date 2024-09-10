1444a634cc0417105c09629bf471af0fa87a1a5c
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-10 16:24:41 +0300

    Add new sizes

--- lib/core/resource/manager_sizes.dart
+++ lib/core/resource/manager_sizes.dart
@@ -10,8 +10,10 @@ class ManagerHeight {
   static double h37 = SizeUitl.setHeight(37);
   static double h44 = SizeUitl.setHeight(44);
   static double h42 = SizeUitl.setHeight(42);
+  static double h48 = SizeUitl.setHeight(48);
   static double h55 = SizeUitl.setHeight(55);
   static double h120 = SizeUitl.setHeight(120);
+  static double h250 = SizeUitl.setHeight(250);
   static double h405 = SizeUitl.setHeight(405);
   static const double infinity = double.infinity;
 }
@@ -19,6 +21,7 @@ class ManagerHeight {
 class ManagerWidth {
   static double w120 = SizeUitl.setWidth(120);
   static double w254 = SizeUitl.setWidth(254);
+  static double w268 = SizeUitl.setWidth(268);
   static double w60 = SizeUitl.setWidth(60);
   static double w38 = SizeUitl.setWidth(38);
   static double w8 = SizeUitl.setWidth(8);
@@ -29,5 +32,6 @@ class ManagerWidth {
 }
 
 class ManagerRadius {
+  static double r5 = SizeUitl.setRadius(5);
   static double r24 = SizeUitl.setRadius(24);
 }
