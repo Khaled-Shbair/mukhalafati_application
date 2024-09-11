19c9e73852c10881e1937d613de11ba1cf1d9fde
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-11 16:07:13 +0300

    Add new Sizes

--- lib/core/resource/manager_sizes.dart
+++ lib/core/resource/manager_sizes.dart
@@ -1,10 +1,13 @@
 import '/config/all_imports.dart';
 
 class ManagerHeight {
+  static double h2 = SizeUitl.setHeight(2);
   static double h5 = SizeUitl.setHeight(5);
   static double h7 = SizeUitl.setHeight(7);
+  static double h10 = SizeUitl.setHeight(10);
   static double h16 = SizeUitl.setHeight(16);
   static double h20 = SizeUitl.setHeight(20);
+  static double h24 = SizeUitl.setHeight(24);
   static double h26 = SizeUitl.setHeight(26);
   static double h30 = SizeUitl.setHeight(30);
   static double h37 = SizeUitl.setHeight(37);
@@ -12,6 +15,9 @@ class ManagerHeight {
   static double h42 = SizeUitl.setHeight(42);
   static double h48 = SizeUitl.setHeight(48);
   static double h55 = SizeUitl.setHeight(55);
+  static double h86 = SizeUitl.setHeight(86);
+  static double h105 = SizeUitl.setHeight(105);
+  static double h117 = SizeUitl.setHeight(117);
   static double h120 = SizeUitl.setHeight(120);
   static double h250 = SizeUitl.setHeight(250);
   static double h405 = SizeUitl.setHeight(405);
@@ -21,13 +27,21 @@ class ManagerHeight {
 class ManagerWidth {
   static double w120 = SizeUitl.setWidth(120);
   static double w254 = SizeUitl.setWidth(254);
+  static double w210 = SizeUitl.setWidth(210);
   static double w268 = SizeUitl.setWidth(268);
+  static double w24 = SizeUitl.setWidth(24);
   static double w60 = SizeUitl.setWidth(60);
+  static double w16 = SizeUitl.setWidth(16);
+  static double w68 = SizeUitl.setWidth(68);
+  static double w70 = SizeUitl.setWidth(70);
+  static double w74 = SizeUitl.setWidth(74);
   static double w38 = SizeUitl.setWidth(38);
+  static double w105 = SizeUitl.setWidth(105);
   static double w8 = SizeUitl.setWidth(8);
   static double w10 = SizeUitl.setWidth(10);
   static double w20 = SizeUitl.setWidth(20);
   static double w55 = SizeUitl.setWidth(55);
+  static double w320 = SizeUitl.setWidth(300);
   static const double infinity = double.infinity;
 }
 
