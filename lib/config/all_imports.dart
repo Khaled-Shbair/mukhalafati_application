1e437500861cdbf733334ef1d92d1ef1321269c0
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-11 16:08:53 +0300

    Add new imports

--- lib/config/all_imports.dart
+++ lib/config/all_imports.dart
@@ -5,6 +5,7 @@ export 'package:flutter_svg/flutter_svg.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:smooth_page_indicator/smooth_page_indicator.dart';
 export 'package:shared_preferences/shared_preferences.dart';
+export 'package:flutter/gestures.dart';
 
 export '/routes/routes.dart';
 export '/routes/route_generator.dart';
@@ -23,6 +24,8 @@ export '/core/resource/manager_strings.dart';
 export '/config/constants/locale_constants.dart';
 export '/core/storage/local/app_settings_shared_preferences.dart';
 export '/core/extensions/extensions.dart';
+export '/core/widgets/main_button.dart';
+export '/core/widgets/main_text_field.dart';
 
 export '/features/splash/presentation/view/screens/splash_screen.dart';
 export '/features/splash/presentation/controller/splash_controller.dart';
@@ -32,5 +35,7 @@ export '/features/on_boarding/presentation/view/widgets/page_view_content.dart';
 export '/features/on_boarding/presentation/view/widgets/skip_button.dart';
 export '/features/on_boarding/presentation/view/widgets/previous_button.dart';
 
-export '/features/login/presentation/view/screens/login_screen.dart';
+export '/features/login/presentation/view/screens/welcome_screen.dart';
 export '/features/login/presentation/controller/login_controller.dart';
+export '/features/login/presentation/view/screens/login_screen.dart';
+export '/features/login/presentation/controller/welcome_controller.dart';
