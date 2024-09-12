d923d7a3b6eafe4c5d3e3c749bc70b31df5e0262
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-12 16:10:21 +0300

    Add background images of screen

--- lib/features/login/presentation/view/screens/welcome_screen.dart
+++ lib/features/login/presentation/view/screens/welcome_screen.dart
@@ -9,87 +9,95 @@ class WelcomeScreen extends StatelessWidget {
       builder: (controller) {
         return Scaffold(
           backgroundColor: ManagerColors.white,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          body: Stack(
             children: [
-              Padding(
-                padding: EdgeInsetsDirectional.only(
-                  start: ManagerWidth.w38,
-                  end: ManagerWidth.w20,
-                  bottom: ManagerHeight.h20,
-                ),
-                child: SvgPicture.asset(
-                  ManagerAssets.login,
-                  width: ManagerWidth.w320,
-                  fit: BoxFit.cover,
-                ),
+              Image.asset(
+                ManagerAssets.backgroundWelcomeScreen,
+                fit: BoxFit.cover,
               ),
-              Text(
-                ManagerStrings.welcome,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: ManagerFontFamily.cairo,
-                  color: ManagerColors.blackOlive,
-                  fontSize: ManagerFontsSizes.f22,
-                  fontWeight: ManagerFontWeight.bold,
-                ),
-              ),
-              Container(
-                margin: EdgeInsetsDirectional.only(
-                  start: ManagerWidth.w70,
-                  end: ManagerWidth.w70,
-                  bottom: ManagerHeight.h55,
-                  top: ManagerHeight.h7,
-                ),
-                child: Text(
-                  ManagerStrings.subTitleLogin,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: ManagerFontFamily.cairo,
-                    color: ManagerColors.blackOlive,
-                    fontSize: ManagerFontsSizes.f14,
-                    fontWeight: ManagerFontWeight.medium,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.only(
+                      start: ManagerWidth.w38,
+                      end: ManagerWidth.w20,
+                      bottom: ManagerHeight.h20,
+                    ),
+                    child: SvgPicture.asset(
+                      ManagerAssets.welcomeImage,
+                      width: ManagerWidth.w268,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsetsDirectional.only(
-                  start: ManagerWidth.w74,
-                  end: ManagerWidth.w74,
-                  bottom: ManagerHeight.h16,
-                ),
-                child: mainButton(
-                  onPressed: () => controller.loginButton(),
-                  child: Text(
-                    ManagerStrings.login,
+                  Text(
+                    ManagerStrings.welcome,
+                    textAlign: TextAlign.center,
                     style: TextStyle(
+                      fontFamily: ManagerFontFamily.cairo,
+                      color: ManagerColors.blackOlive,
+                      fontSize: ManagerFontsSizes.f22,
+                      fontWeight: ManagerFontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsetsDirectional.only(
+                      start: ManagerWidth.w70,
+                      end: ManagerWidth.w70,
+                      bottom: ManagerHeight.h55,
+                      top: ManagerHeight.h7,
+                    ),
+                    child: Text(
+                      ManagerStrings.subTitleLogin,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: ManagerFontFamily.cairo,
+                        color: ManagerColors.blackOlive,
+                        fontSize: ManagerFontsSizes.f14,
+                        fontWeight: ManagerFontWeight.medium,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.only(
+                      start: ManagerWidth.w74,
+                      end: ManagerWidth.w74,
+                      bottom: ManagerHeight.h16,
+                    ),
+                    child: mainButton(
+                      onPressed: () => controller.loginButton(),
+                      child: Text(
+                        ManagerStrings.login,
+                        style: TextStyle(
+                          color: ManagerColors.white,
+                          fontFamily: ManagerFontFamily.cairo,
+                          fontWeight: ManagerFontWeight.bold,
+                          fontSize: ManagerFontsSizes.f18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.only(
+                      start: ManagerWidth.w74,
+                      end: ManagerWidth.w74,
+                    ),
+                    child: mainButton(
                       color: ManagerColors.white,
-                      fontFamily: ManagerFontFamily.cairo,
-                      fontWeight: ManagerFontWeight.bold,
-                      fontSize: ManagerFontsSizes.f18,
+                      onPressed: () => controller.createAccountButton(),
+                      child: Text(
+                        ManagerStrings.createAccount,
+                        style: TextStyle(
+                          color: ManagerColors.black,
+                          fontFamily: ManagerFontFamily.cairo,
+                          fontWeight: ManagerFontWeight.bold,
+                          fontSize: ManagerFontsSizes.f18,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsetsDirectional.only(
-                  start: ManagerWidth.w74,
-                  end: ManagerWidth.w74,
-                ),
-                child: mainButton(
-                  color: ManagerColors.white,
-                  onPressed: () => controller.createAccountButton(),
-                  child: Text(
-                    ManagerStrings.createAccount,
-                    style: TextStyle(
-                      color: ManagerColors.black,
-                      fontFamily: ManagerFontFamily.cairo,
-                      fontWeight: ManagerFontWeight.bold,
-                      fontSize: ManagerFontsSizes.f18,
-                    ),
-                  ),
-                ),
+                ],
               ),
             ],
           ),
