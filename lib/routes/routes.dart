befca8feda965c0560fa26ab21c89bfc903b17ee
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-12 16:07:18 +0300

    Add new routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,5 +3,10 @@ class Routes {
   static const String onBoardingScreen = 'on_boarding_Screen';
   static const String welcomeScreen = 'welcome_Screen';
   static const String loginScreen = 'login_Screen';
-  static const String registerScreen = 'register_Screen';
+  static const String registerPoliceManScreen = 'register_police_man_Screen';
+  static const String registerDriverScreen = 'register_driver_Screen';
+  static const String forgetPoliceManPasswordScreen =
+      'forget_Police_man_password_Screen';
+  static const String forgetDriverPasswordScreen =
+      'forget_driver_password_Screen';
 }
