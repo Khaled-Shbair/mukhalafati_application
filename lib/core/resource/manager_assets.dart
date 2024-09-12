4e8b1d9e65e833b7e2dc55086dcfa9535b536656
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-12 16:13:12 +0300

    Add path of new images

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -10,7 +10,9 @@ class ManagerAssets {
       '$_imagePath/on_boarding/on_boarding_two.png';
   static const String onBoardingThree =
       '$_imagePath/on_boarding/on_boarding_three.png';
-  static const String login = '$_imagePath/login/login.svg';
-  static const String driverIcon = '$_imagePath/login/driver_icon.svg';
+  static const String welcomeImage = '$_imagePath/login/welcome_image.svg';
+  static const String backgroundWelcomeScreen =
+      '$_imagePath/login/background_welcome_screen.png';
+  static const String driverIcon = '$_imagePath/login/driver_icon.png';
   static const String policeIcon = '$_imagePath/login/police_icon.png';
 }
