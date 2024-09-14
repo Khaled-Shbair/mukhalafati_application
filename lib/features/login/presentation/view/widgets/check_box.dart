ee4faf84dbf488ce7eff636b869af6521d550190
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-14 15:04:28 +0300

    Create CheckBox Widget

---
+++ lib/features/login/presentation/view/widgets/check_box.dart
@@ -0,0 +1,37 @@
+import '/config/all_imports.dart';
+
+Widget checkBox({
+  required bool value,
+  required Function(bool? v) onChanged,
+}) {
+  return Row(
+    children: [
+      Checkbox(
+        hoverColor: Colors.green,
+        focusColor: Colors.blue,
+        value: value,
+        onChanged: onChanged,
+        checkColor: ManagerColors.white,
+        activeColor: ManagerColors.primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(ManagerRadius.r5),
+        ),
+        side: const BorderSide(
+          color: ManagerColors.primaryColor,
+          width: 1.5,
+        ),
+        splashRadius: AppConstants.splashRadiusOfCheckboxInLoginScreen,
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+      Text(
+        ManagerStrings.rememberMe,
+        style: TextStyle(
+          fontFamily: ManagerFontFamily.cairo,
+          fontSize: ManagerFontsSizes.f13,
+          fontWeight: ManagerFontWeight.medium,
+          color: ManagerColors.philippineGray,
+        ),
+      ),
+    ],
+  );
+}
