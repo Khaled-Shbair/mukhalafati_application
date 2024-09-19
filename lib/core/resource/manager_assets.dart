9393465ecb1ee38fd8b50e5123399835643e7e6e
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-19 14:54:52 +0300

    Add new paths of images assets

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -2,6 +2,7 @@ const String _imagePath = 'assets/images';
 
 class ManagerAssets {
   static const String logo = '$_imagePath/logo.png';
+  static const String userIcon = '$_imagePath/user_icon.png';
   static const String createdSuccessfully =
       '$_imagePath/created_successfully.svg';
   static const String logoSplash = '$_imagePath/splash/logo_splash.png';
@@ -20,4 +21,8 @@ class ManagerAssets {
   static const String policeIcon = '$_imagePath/login/police_icon.png';
   static const String palestineLogo =
       '$_imagePath/violation/palestine_logo.png';
+  static const String emptyResultSearch = '$_imagePath/search/empty_result_search.svg';
+  static const String resultOfLicense = '$_imagePath/search/result_of_license.svg';
+  static const String searchIcon = '$_imagePath/search/search_icon.svg';
+
 }
