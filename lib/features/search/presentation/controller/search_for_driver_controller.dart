ac734f74495f5ae8fc90a6e52053e811d39f7489
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-23 15:04:40 +0300

    Add policeManDrawer in this screens and edits some code

--- lib/features/search/presentation/controller/search_for_driver_controller.dart
+++ lib/features/search/presentation/controller/search_for_driver_controller.dart
@@ -1,13 +1,15 @@
 import '/config/all_imports.dart';
 
 class SearchForDriverController extends GetxController {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   late TextEditingController licenseNumberController;
   bool loading = false;
-  bool result = true;
-  String licenseNumber = '0015440';
+  bool result = false;
+  String licenseNumber = '10203040';
   String expiryDate = '10-02-2024';
   String releaseDate = '22-02-2029';
-  String idNumber = '802075986';
+  String idNumber = '12345678';
   String nameAr = 'محمد عبد الحليم عبد الفتاح مسعود';
   String nameEn = 'Mohammed A.A.Massoud';
   String licenseLevels = '[2  3  B]';
@@ -16,7 +18,8 @@ class SearchForDriverController extends GetxController {
       'https://the-stock-products.s3.us-east-2.amazonaws.com/display_images/displayf004fcf1ed2fceb7dbb63496564d0386.jpg';
 
   // String imageDriver = 'https://upload.wikimedia.org/wikipedia/commons/4/44/%D8%B5%D9%88%D8%B1%D8%A9_%D8%B4%D8%AE%D8%B5%D9%8A%D8%A9_%D9%88%D9%84%D9%8A%D8%AF_%D8%A7%D9%84%D8%AD%D8%B4%D9%81.jpg';
-int numberOfViolations = 15;
+  int numberOfViolations = 15;
+
   @override
   void onInit() {
     super.onInit();
@@ -29,5 +32,21 @@ int numberOfViolations = 15;
     super.onClose();
   }
 
-  void searchButton() {}
+  void openEndDrawer() {
+    if (scaffoldKey.currentState != null &&
+        !scaffoldKey.currentState!.isEndDrawerOpen) {
+      scaffoldKey.currentState!.openEndDrawer();
+    }
+  }
+
+  void searchButton() {
+    loading = true;
+    if (licenseNumberController.text == '10203040') {
+      result = true;
+    } else {
+      result = false;
+    }
+    loading = false;
+    update();
+  }
 }
