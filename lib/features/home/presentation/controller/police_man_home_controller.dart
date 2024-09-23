065f5ce1670a0baf3ec56085ede7cd5c1d049c4d
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-23 15:05:45 +0300

    Add policeManDrawer and edit on design (complete later)

--- lib/features/home/presentation/controller/police_man_home_controller.dart
+++ lib/features/home/presentation/controller/police_man_home_controller.dart
@@ -3,10 +3,19 @@ import '/config/all_imports.dart';
 class PoliceManHomeController extends GetxController
     with GetSingleTickerProviderStateMixin {
   late TabController tabController;
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final String totalViolations = '999';
 
   @override
   void onInit() {
     super.onInit();
     tabController = TabController(length: 3, vsync: this);
+  }
+
+  void openEndDrawer() {
+    if (scaffoldKey.currentState != null &&
+        !scaffoldKey.currentState!.isEndDrawerOpen) {
+      scaffoldKey.currentState!.openEndDrawer();
+    }
   }
 }
