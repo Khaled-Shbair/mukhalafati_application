9062aad1923bf7d13480e2e396d9a08d3a330da5
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-24 15:54:33 +0300

    Create DataOfComplaints model

---
+++ lib/features/complaints/presentation/model/data_of_complaints.dart
@@ -0,0 +1,11 @@
+class DataOfComplaints {
+  final String date;
+  final String text;
+  final bool state;
+
+  DataOfComplaints({
+    required this.date,
+    required this.text,
+    required this.state,
+  });
+}
