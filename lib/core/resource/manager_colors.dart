b7c6e780931440ee6a2aec817f13e7e4acd9e66a
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-24 15:52:33 +0300

    Add new colors

--- lib/core/resource/manager_colors.dart
+++ lib/core/resource/manager_colors.dart
@@ -10,24 +10,30 @@ class ManagerColors {
   static const Color gunmetal = Color.fromRGBO(38, 50, 56, .80);
   static const Color grannySmithApple = Color.fromRGBO(161, 217, 155, .70);
   static const Color begonia = Color.fromRGBO(247, 109, 109, .70);
-  static const Color black5 = Color.fromRGBO(0, 0, 0, .05); //black opacity 5%
-  static const Color black25 = Color.fromRGBO(0, 0, 0, .25); //black opacity 25%
-  static const Color black50 = Color.fromRGBO(0, 0, 0, .50); //black opacity 50%
+  static const Color peach = Color.fromRGBO(255, 229, 180, .70);
 
   static const Color mayGreen = Color(0XFF4F8F48);
   static const Color bittersweetShimmer = Color(0XFFBE5151);
+  static const Color harvestGold = Color(0XFFD28801);
   static const Color blackOlive = Color(0XFF404040);
   static const Color white = Color(0XFFFFFFFF);
-  static const Color black = Color(0XFF000000);
   static const Color davyGrey = Color(0XFF595959);
   static const Color dimGray = Color(0XFF696969);
   static const Color lightSilver = Color(0XFFD9D9D9);
   static const Color darkPurple = Color(0XFF230B34);
   static const Color ghostWhite = Color(0XFFF9F9F9);
   static const Color lotion = Color(0XFFFBFBFB);
+  static const Color platinum = Color(0XFFE8E8E8);
   static const Color eerieBlack = Color(0XFF1C1C1C);
   static const Color antiFlashWhite = Color(0XFFF3F4F6);
   static const Color darkGunmetal = Color(0XFF182233);
-  static const Color  spanishGray = Color(0XFF979797);
+  static const Color spanishGray = Color(0XFF979797);
   static const Color transparent = Colors.transparent;
+
+  static const Color black = Color(0XFF000000);
+  static const Color black5 = Color.fromRGBO(0, 0, 0, .05); //black opacity 5%
+  static const Color black25 = Color.fromRGBO(0, 0, 0, .25); //black opacity 25%
+  static const Color black50 = Color.fromRGBO(0, 0, 0, .50); //black opacity 50%
+  static const Color black70 = Color.fromRGBO(0, 0, 0, .70); //black opacity 70%
+
 }
