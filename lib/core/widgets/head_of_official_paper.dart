5f2682e8ea473d6550ecf97d4fea6a5bfd4bc024
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-24 15:53:01 +0300

    Create headOfOfficialPaper widget

---
+++ lib/core/widgets/head_of_official_paper.dart
@@ -0,0 +1,37 @@
+import '/config/all_imports.dart';
+
+Widget headOfOfficialPaper() {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      _textOfHeadOfficialPaper(ManagerStrings.titleHeadViolationAr),
+      Image.asset(ManagerAssets.palestineLogo),
+      _textOfHeadOfficialPaper(ManagerStrings.titleHeadViolationEn),
+    ],
+  );
+}
+
+Widget _textOfHeadOfficialPaper(String text) {
+  return Container(
+    margin: EdgeInsetsDirectional.only(
+      start: ManagerWidth.w5,
+      end: ManagerWidth.w5,
+    ),
+    child: Text(
+      text,
+      textAlign: TextAlign.center,
+      textScaler: const TextScaler.linear(0.9),
+      overflow: TextOverflow.ellipsis,
+      maxLines: 3,
+      style: TextStyle(
+        fontFamily: ManagerFontFamily.cairo,
+        fontSize: ManagerFontsSizes.f8,
+        color: ManagerColors.black,
+        fontWeight: ManagerFontWeight.bold,
+        height: ManagerHeight.h2,
+        decoration: TextDecoration.none,
+        decorationColor: ManagerColors.transparent,
+      ),
+    ),
+  );
+}
