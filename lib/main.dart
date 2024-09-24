e0ea10c820e0c96772bb7d8f412c7d29f10bdf0c
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-24 15:54:49 +0300

    Create borderSideOfComplaintsTable

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: Routes.searchForDriverScreen,
+          initialRoute: Routes.listOfComplaintsScreen,
           onGenerateRoute: RouteGenerator.getRoute,
           translations: Translation(),
           locale: const Locale(LocaleConstants.arabicLanguage),
