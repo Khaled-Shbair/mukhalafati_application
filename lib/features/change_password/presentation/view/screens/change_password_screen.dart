8ddd7d74231b06a6e3ba0ad0730843394de42b1f
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-26 14:38:56 +0300

    Create ChangePassword Screen

---
+++ lib/features/change_password/presentation/view/screens/change_password_screen.dart
@@ -0,0 +1,103 @@
+import '/config/all_imports.dart';
+
+class ChangePasswordScreen extends StatelessWidget {
+  const ChangePasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<ChangePasswordController>(
+      builder: (controller) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () => controller.backButton(),
+              icon: const Icon(Icons.arrow_back_ios),
+            ),
+            title: Text(ManagerStrings.changePassword),
+          ),
+          body: ListView(
+            physics: const NeverScrollableScrollPhysics(),
+            padding: EdgeInsetsDirectional.only(
+              start: ManagerWidth.w28,
+              end: ManagerWidth.w28,
+              top: ManagerHeight.h34,
+            ),
+            children: [
+              SvgPicture.asset(
+                ManagerAssets.changePasswordImage,
+                height: ManagerHeight.h182,
+                width: ManagerWidth.infinity,
+              ),
+              SizedBox(height: ManagerHeight.h50),
+              Text(
+                ManagerStrings.enterNewPassword,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: ManagerColors.black,
+                  fontFamily: ManagerFontFamily.cairo,
+                  fontWeight: ManagerFontWeight.bold,
+                  fontSize: ManagerFontsSizes.f15,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.only(
+                  top: ManagerHeight.h8,
+                  bottom: ManagerHeight.h20,
+                  start: ManagerWidth.w14,
+                  end: ManagerWidth.w14,
+                ),
+                child: Text(
+                  ManagerStrings
+                      .pleaseEnterNewPasswordWhichMustBeDifferentFromTheOldPassword,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    height: 2,
+                    color: ManagerColors.davyGrey,
+                    fontFamily: ManagerFontFamily.cairo,
+                    fontWeight: ManagerFontWeight.medium,
+                    fontSize: ManagerFontsSizes.f13,
+                  ),
+                ),
+              ),
+              SizedBox(height: ManagerHeight.h8),
+              mainTextField(
+                controller: controller.newPassword,
+                labelText: ManagerStrings.newPassword,
+                colorLabelText: ManagerColors.quartz,
+                isPassword: true,
+                obscureText: controller.obscureNewPassword,
+                keyboardType: TextInputType.visiblePassword,
+                changeObscureText: () => controller.changeObscureNewPassword(),
+              ),
+              SizedBox(height: ManagerHeight.h15),
+              mainTextField(
+                controller: controller.confirmPassword,
+                labelText: ManagerStrings.confirmPassword,
+                colorLabelText: ManagerColors.quartz,
+                obscureText: controller.obscureConfirmPassword,
+                isPassword: true,
+                keyboardType: TextInputType.visiblePassword,
+                changeObscureText: () =>
+                    controller.changeObscureConfirmPassword(),
+              ),
+              SizedBox(height: ManagerHeight.h24),
+              mainButton(
+                onPressed: () => controller.changePasswordButton(),
+                child: Text(
+                  ManagerStrings.changePassword,
+                  style: TextStyle(
+                    color: ManagerColors.white,
+                    fontFamily: ManagerFontFamily.cairo,
+                    fontWeight: ManagerFontWeight.bold,
+                    fontSize: ManagerFontsSizes.f16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
