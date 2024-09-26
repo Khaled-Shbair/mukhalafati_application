24fbcef98ab5cc01ab342b2f8d1128df84d3a924
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-26 13:55:18 +0300

    Create VerificationCode Controller

---
+++ lib/features/forget_password/presentation/controller/verification_code_controller.dart
@@ -0,0 +1,100 @@
+import '/config/all_imports.dart';
+
+class VerificationCodeController extends GetxController {
+  late TextEditingController oneNumberOfCode;
+  late TextEditingController twoNumberOfCode;
+  late TextEditingController threeNumberOfCode;
+  late TextEditingController fourNumberOfCode;
+  late FocusNode oneFocusNode;
+  late FocusNode twoFocusNode;
+  late FocusNode threeFocusNode;
+  late FocusNode fourFocusNode;
+  bool changeBorderColorOneFiled = false;
+  bool changeBorderColorTwoFiled = false;
+  bool changeBorderColorThreeFiled = false;
+  bool changeBorderColorFourFiled = false;
+
+  bool returnCodeIsInCorrect = false;
+
+  @override
+  void onInit() {
+    super.onInit();
+    oneNumberOfCode = TextEditingController();
+    twoNumberOfCode = TextEditingController();
+    threeNumberOfCode = TextEditingController();
+    fourNumberOfCode = TextEditingController();
+    oneFocusNode = FocusNode();
+    twoFocusNode = FocusNode();
+    threeFocusNode = FocusNode();
+    fourFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    oneNumberOfCode.dispose();
+    twoNumberOfCode.dispose();
+    threeNumberOfCode.dispose();
+    fourNumberOfCode.dispose();
+    oneFocusNode.dispose();
+    twoFocusNode.dispose();
+    threeFocusNode.dispose();
+    fourFocusNode.dispose();
+    super.dispose();
+  }
+
+  void backButton() {
+    Get.back();
+    disposeVerificationCode();
+  }
+
+  String phoneNumber = '0599724073';
+
+  void verifyButton() {
+    Get.toNamed(Routes.changePasswordScreen);
+    disposeVerificationCode();
+    disposeForgotPasswordForDriver();
+    disposeForgotPasswordForPoliceMan();
+  }
+
+  void onChangeOneFiled(value) {
+    if (value.isNotEmpty) {
+      changeBorderColorOneFiled = true;
+      twoFocusNode.requestFocus();
+    } else {
+      changeBorderColorOneFiled = false;
+    }
+    update();
+  }
+
+  void onChangeTwoFiled(value) {
+    if (value.isNotEmpty) {
+      changeBorderColorTwoFiled = true;
+      threeFocusNode.requestFocus();
+    } else {
+      changeBorderColorTwoFiled = false;
+      oneFocusNode.requestFocus();
+    }
+    update();
+  }
+
+  void onChangeThreeFiled(value) {
+    if (value.isNotEmpty) {
+      changeBorderColorThreeFiled = true;
+      fourFocusNode.requestFocus();
+    } else {
+      changeBorderColorThreeFiled = false;
+      twoFocusNode.requestFocus();
+    }
+    update();
+  }
+
+  void onChangeFourFiled(value) {
+    if (value.isNotEmpty) {
+      changeBorderColorFourFiled = true;
+    } else {
+      changeBorderColorFourFiled = false;
+      threeFocusNode.requestFocus();
+    }
+    update();
+  }
+}
