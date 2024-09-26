3cd0e217bc5a22dca8e18ea1adae908aef9d471a
Khaled Shbair <khaled.shbair12@gmail.com>
2024-09-26 15:37:05 +0300

    Add new constants

--- lib/config/constants/app_constants.dart
+++ lib/config/constants/app_constants.dart
@@ -15,7 +15,8 @@ class AppConstants {
   static const double blurRadiusOfBoxShadowInCreatedSuccessfullyWidget = 6;
   static const double spreadRadiusOfBoxShadowInCreatedSuccessfullyWidget = 6;
   static const double blurRadiusOfBoxShadowInConfirmInformationWidget = 6;
-  static const double blurRadiusOfBoxShadowInTabBarViewInPoliceManHomeScreen  = 4;
+  static const double blurRadiusOfBoxShadowInTabBarViewInPoliceManHomeScreen =
+      4;
   static const double spreadRadiusOfBoxShadowInLogoutScreen = 6;
   static const double blurRadiusOfBoxShadowInLogoutScreen = 6;
   static const double spreadRadiusOfBoxShadowInListOfComplaintsScreen = 3;
@@ -23,6 +24,7 @@ class AppConstants {
   static const double spreadRadiusOfBoxShadowInConfirmInformationWidget = 2;
   static const double blurRadiusOfBoxShadowInResultOfDriverWidget = 2;
   static const int maxLengthOfLicenseNumber = 8;
+  static const int maxLengthOfPhoneNumber = 10;
   static const int maxLengthOfJobNumber = 8;
   static const int maxLengthOfVehicleNumber = 8;
   static const int maxLengthOfIDNumber = 8;
