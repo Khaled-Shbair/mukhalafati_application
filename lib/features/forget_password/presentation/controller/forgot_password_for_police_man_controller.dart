782aafb0e8f90d92015a87c250ea94506b698eef
Khaled Shbair <khaled.shbair12@gmail.com>
2024-10-03 16:00:37 +0300

    Enhance and check on code in forgot_password_for_police_man_controller.dart

--- lib/features/forget_password/presentation/controller/forgot_password_for_police_man_controller.dart
+++ lib/features/forget_password/presentation/controller/forgot_password_for_police_man_controller.dart
@@ -1,6 +1,6 @@
 import '/config/all_imports.dart';
 
-class ForgotPasswordForPoliceManController extends GetxController {
+class ForgotPasswordForPoliceManController extends GetxController with Helpers {
   late TextEditingController jobNumber;
 
   @override
@@ -17,8 +17,31 @@ class ForgotPasswordForPoliceManController extends GetxController {
 
   void backButton() {
     Get.back();
-    disposeForgotPasswordForDriver();
+    disposeForgotPasswordForPoliceMan();
   }
 
-  void sendButton() {}
+
+  void sendButton() {
+    //TODO: Later Edit phone number get from Api
+    if (_checkDataDriver()) {
+      String phoneNumber = '0599724037';
+      phoneNumber =
+          // '${number.characters.characterAt(0)}${number.characters.characterAt(1)}${number.characters.characterAt(2)}*****${number.characters.characterAt(8)}${number.characters.characterAt(9)}';
+          '${phoneNumber.characters.characterAt(8)}${phoneNumber.characters.characterAt(9)}*****${phoneNumber.characters.characterAt(0)}${phoneNumber.characters.characterAt(1)}${phoneNumber.characters.characterAt(2)}';
+      Get.toNamed(
+        Routes.verificationCodeScreen,
+        arguments: phoneNumber,
+      );
+    } else {
+      showSnackBar(message: ManagerStrings.pleaseEnterTheRequiredData);
+    }
+  }
+
+  bool _checkDataDriver() {
+    if (jobNumber.text.isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
