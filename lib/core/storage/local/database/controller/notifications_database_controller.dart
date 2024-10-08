cec329ebf69026292593906c0d0b11162e50d822
Khaled Shbair <khaled.shbair12@gmail.com>
2024-10-08 15:17:12 +0300

    Create NotificationsDatabaseController to control all function

---
+++ lib/core/storage/local/database/controller/notifications_database_controller.dart
@@ -0,0 +1,52 @@
+import '/config/all_imports.dart';
+
+class NotificationsDatabaseController
+    extends DatabaseOperations<NotificationModel> {
+  final Database _database = DatabaseProvider().database;
+
+  @override
+  Future<int> create(NotificationModel object) async {
+    int newRowId = await _database.insert(
+      DatabaseConstants.notificationsTableName,
+      object.toMap(),
+    );
+    return newRowId;
+  }
+
+  @override
+  Future<bool> delete(int id) async {
+    int countOfDeletedRows = await _database.delete(
+      DatabaseConstants.notificationsTableName,
+      where: '${DatabaseConstants.notificationId} = ?',
+      whereArgs: [id],
+    );
+    return countOfDeletedRows == 1;
+  }
+
+  Future<bool>clear() async {
+    int countOfDeletedRows = await _database.delete(
+      DatabaseConstants.notificationsTableName,
+    );
+    return countOfDeletedRows == 1;
+  }
+
+  @override
+  Future<List<NotificationModel>> read([int? id]) async {
+    List<Map<String, dynamic>> rows = await _database.query(
+      DatabaseConstants.notificationsTableName,
+    );
+    return rows.map((row) => NotificationModel.fromMap(row)).toList();
+  }
+
+  @override
+  Future<NotificationModel> show(String id) {
+    // TODO: implement show
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> update(object) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
