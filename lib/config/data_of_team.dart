9a15b738de5cd707ebfcee4b3da8027c951a1888
Khaled Shbair <khaled.shbair12@gmail.com>
2024-10-19 15:03:52 +0300

    Add new data in database

--- lib/config/data_of_team.dart
+++ lib/config/data_of_team.dart
@@ -1,5 +1,32 @@
 import 'all_imports.dart';
 
+void createDoctorAccount() async {
+  DriverDatabaseController driverDatabase =
+      instance<DriverDatabaseController>();
+  DriverModel doctor = DriverModel();
+  doctor.driverIdNumber = '968892830';
+  doctor.password = '0000';
+  doctor.driverImage = ManagerAssets.doctor;
+  doctor.licenseNumber = '987654321';
+  doctor.licenseLevels = '[2 3 B]';
+  doctor.expiryDate = '28-9-2028';
+  doctor.releaseDate = '25-9-2023';
+  doctor.phone = '0595668801';
+  doctor.driverFirstNameEn = 'Mohamed';
+  doctor.driverFirstNameAr = 'محمد';
+  doctor.driverFatherNameAr = 'زكي';
+  doctor.driverGrandfatherNameAr = 'دهمان';
+  doctor.driverLastNameAr = 'شبير';
+  doctor.driverFatherNameEn = 'Z.';
+  doctor.driverGrandfatherNameEn = 'D.';
+  doctor.driverNameEn = 'Mohamed Z.D. Shbair';
+  doctor.driverLastNameEn = 'Shbair';
+  doctor.numberOfUnReadNotifications = 2;
+  doctor.numberOfViolationsUnPaid = 1;
+  doctor.numberOfViolationsPaid = 1;
+  await driverDatabase.create(doctor);
+}
+
 void createSajaAccount() async {
   DriverDatabaseController driverDatabase =
       instance<DriverDatabaseController>();
@@ -21,7 +48,9 @@ void createSajaAccount() async {
   saja.driverLastNameAr = 'الأشرم';
   saja.driverNameEn = 'Saja F.S. AlAshram';
   saja.driverLastNameEn = 'ALAshram';
-  saja.numberOfUnReadNotifications = 3;
+  saja.numberOfUnReadNotifications = 2;
+  saja.numberOfViolationsUnPaid = 1;
+  saja.numberOfViolationsPaid = 1;
   await driverDatabase.create(saja);
 }
 
@@ -33,6 +62,7 @@ void createMalakAccount() async {
   malak.password = '0000';
 
   malak.driverImage = ManagerAssets.malak;
+  malak.numberOfViolationsUnPaid = 10;
   malak.licenseNumber = '96578450';
   malak.licenseLevels = '[2 1 B]';
   malak.expiryDate = '28-9-2028';
@@ -47,7 +77,9 @@ void createMalakAccount() async {
   malak.driverLastNameAr = 'مسعود';
   malak.driverNameEn = 'Malak A.A. Massoud';
   malak.driverLastNameEn = 'Massoud';
-  malak.numberOfUnReadNotifications = 6;
+  malak.numberOfUnReadNotifications = 2;
+  malak.numberOfViolationsPaid = 1;
+  malak.numberOfViolationsUnPaid = 1;
   await driverDatabase.create(malak);
 }
 
@@ -57,6 +89,8 @@ void createGeneenAccount() async {
   DriverModel geneen = DriverModel();
   geneen.driverIdNumber = '407811736';
   geneen.password = '0000';
+  geneen.numberOfViolationsUnPaid = 10;
+  geneen.numberOfViolationsPaid = 7;
   geneen.driverImage = ManagerAssets.geneen;
   geneen.licenseNumber = '96578426';
   geneen.licenseLevels = '[2 3 B]';
@@ -72,7 +106,9 @@ void createGeneenAccount() async {
   geneen.driverLastNameAr = 'الجليس';
   geneen.driverNameEn = 'Geneen M.M. Aljalees';
   geneen.driverLastNameEn = 'Aljalees';
-  geneen.numberOfUnReadNotifications = 1;
+  geneen.numberOfUnReadNotifications = 2;
+  geneen.numberOfViolationsPaid = 1;
+  geneen.numberOfViolationsUnPaid = 1;
   await driverDatabase.create(geneen);
 }
 
@@ -80,6 +116,8 @@ void createShahedAccount() async {
   DriverDatabaseController driverDatabase =
       instance<DriverDatabaseController>();
   DriverModel shahed = DriverModel();
+  shahed.numberOfViolationsUnPaid = 10;
+  shahed.numberOfViolationsPaid = 7;
   shahed.driverIdNumber = '407811739';
   shahed.driverImage = ManagerAssets.shahed;
   shahed.licenseNumber = '96578443';
@@ -97,7 +135,9 @@ void createShahedAccount() async {
   shahed.driverLastNameAr = 'شكوكاني';
   shahed.driverNameEn = 'Shahed A.A. Shkokani';
   shahed.driverLastNameEn = 'Shkokani';
-  shahed.numberOfUnReadNotifications = 9;
+  shahed.numberOfUnReadNotifications = 2;
+  shahed.numberOfViolationsPaid = 1;
+  shahed.numberOfViolationsUnPaid = 1;
   await driverDatabase.create(shahed);
 }
 
@@ -114,5 +154,129 @@ void createPoliceAccount() async {
   police.policePhoneNumber = '0599724037';
   police.policeFatherNameAr = 'عبد المنعم';
   police.policeGrandfatherNameAr = 'عثمان';
-  await policeDatabase.create(police);
+  int r = await policeDatabase.create(police);
+  debugPrint('Police: $r');
+}
+
+void createTest() async {
+  TestResultDatabaseController testResultDatabase =
+      instance<TestResultDatabaseController>();
+  TestResultModel test = TestResultModel();
+  test.studentId = '12345678';
+  test.studentName = 'خالد عبد المنعم عثمان شبير';
+  test.licenseTestResults = 0;
+  test.practicalTestResults = 1;
+  int x = await testResultDatabase.create(test);
+  debugPrint('Test Result: $x');
+}
+
+void s() async {
+  ViolationsDatabaseController violationsDatabase =
+      instance<ViolationsDatabaseController>();
+  ViolationModel a = ViolationModel();
+  a.violationDate = '17/9/2024';
+  a.violationTime = '10:49:30 AM';
+  a.priceOfViolation = 200;
+  a.violationState = 1;
+  a.violationReason = 'الوقوف في مكان ممنوع';
+  a.violationAddress = 'خانيونس - البلد - شارع شبير';
+  a.violationPayedBy = ManagerStrings.jawwalPay;
+  a.driverId = 1;
+  a.policeId = 12345678;
+  await violationsDatabase.create(a);
+  ViolationModel aa = ViolationModel();
+  aa.violationDate = '17/9/2024';
+  aa.violationTime = '2:49:30 PM';
+  aa.priceOfViolation = 250;
+  aa.violationState = 0;
+  aa.violationReason = 'عدم وضع حزام الأمان';
+  aa.violationAddress = 'خانيونس - دوار بني سهيلا';
+  aa.driverId = 1;
+  aa.policeId = 12345678;
+  await violationsDatabase.create(aa);
+
+  ViolationModel b = ViolationModel();
+  b.violationDate = '17/9/2024';
+  b.violationTime = '10:49:30 AM';
+  b.priceOfViolation = 200;
+  b.violationState = 1;
+  b.violationReason = 'الوقوف في مكان ممنوع';
+  b.violationAddress = 'خانيونس - البلد - شارع شبير';
+  b.violationPayedBy = ManagerStrings.jawwalPay;
+  b.driverId = 2;
+  b.policeId = 12345678;
+  await violationsDatabase.create(b);
+  ViolationModel bb = ViolationModel();
+  bb.violationDate = '17/9/2024';
+  bb.violationTime = '2:49:30 PM';
+  bb.priceOfViolation = 250;
+  bb.violationState = 0;
+  bb.violationReason = 'عدم وضع حزام الأمان';
+  bb.violationAddress = 'خانيونس - دوار بني سهيلا';
+  bb.driverId = 2;
+  bb.policeId = 12345678;
+  await violationsDatabase.create(bb);
+  ViolationModel c = ViolationModel();
+  c.violationDate = '17/9/2024';
+  c.violationTime = '10:49:30 AM';
+  c.priceOfViolation = 200;
+  c.violationState = 1;
+  c.violationReason = 'الوقوف في مكان ممنوع';
+  c.violationAddress = 'خانيونس - البلد - شارع شبير';
+  c.violationPayedBy = ManagerStrings.jawwalPay;
+  c.driverId = 3;
+  c.policeId = 12345678;
+  await violationsDatabase.create(c);
+  ViolationModel cc = ViolationModel();
+  cc.violationDate = '17/9/2024';
+  cc.violationTime = '2:49:30 PM';
+  cc.priceOfViolation = 250;
+  cc.violationState = 0;
+  cc.violationReason = 'عدم وضع حزام الأمان';
+  cc.violationAddress = 'خانيونس - دوار بني سهيلا';
+  cc.driverId = 3;
+  cc.policeId = 12345678;
+  await violationsDatabase.create(cc);
+  ViolationModel d = ViolationModel();
+  d.violationDate = '17/9/2024';
+  d.violationTime = '10:49:30 AM';
+  d.priceOfViolation = 200;
+  d.violationState = 1;
+  d.violationReason = 'الوقوف في مكان ممنوع';
+  d.violationAddress = 'خانيونس - البلد - شارع شبير';
+  d.violationPayedBy = ManagerStrings.jawwalPay;
+  d.driverId = 4;
+  d.policeId = 12345678;
+  await violationsDatabase.create(d);
+  ViolationModel dd = ViolationModel();
+  dd.violationDate = '17/9/2024';
+  dd.violationTime = '2:49:30 PM';
+  dd.priceOfViolation = 250;
+  dd.violationState = 0;
+  dd.violationReason = 'عدم وضع حزام الأمان';
+  dd.violationAddress = 'خانيونس - دوار بني سهيلا';
+  dd.driverId = 4;
+  dd.policeId = 12345678;
+  await violationsDatabase.create(dd);
+  ViolationModel e = ViolationModel();
+  e.violationDate = '17/9/2024';
+  e.violationTime = '10:49:30 AM';
+  e.priceOfViolation = 200;
+  e.violationState = 1;
+  e.violationReason = 'الوقوف في مكان ممنوع';
+  e.violationAddress = 'خانيونس - البلد - شارع شبير';
+  e.violationPayedBy = ManagerStrings.jawwalPay;
+  e.driverId = 5;
+  e.policeId = 12345678;
+  await violationsDatabase.create(e);
+  ViolationModel ee = ViolationModel();
+  ee.violationDate = '17/9/2024';
+  ee.violationTime = '2:49:30 PM';
+  ee.priceOfViolation = 250;
+  ee.violationState = 0;
+  ee.violationReason = 'عدم وضع حزام الأمان';
+  ee.violationAddress = 'خانيونس - دوار بني سهيلا';
+  ee.driverId = 5;
+  ee.policeId = 12345678;
+  await violationsDatabase.create(ee);
 }
