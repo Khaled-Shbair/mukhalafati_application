c14e2263eb7c86a8de7696acef29b13b5cb2bdf5
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-05 11:51:36 +0200

    Create PoliceManLoginModel and DriverLoginModel to save response data in domain layer

---
+++ lib/features/login/domain/model/police_man_login_model.dart
@@ -0,0 +1,23 @@
+class PoliceManLoginModel {
+  int? id;
+  String? image;
+  String? firstNameAr;
+  String? lastNameAr;
+  String? fatherNameAr;
+  String? grandFatherNameAr;
+  String? phoneNumber;
+  String? jobNumber;
+  String? militaryRank;
+
+  PoliceManLoginModel({
+    required this.id,
+    required this.militaryRank,
+    required this.jobNumber,
+    required this.image,
+    required this.lastNameAr,
+    required this.grandFatherNameAr,
+    required this.fatherNameAr,
+    required this.firstNameAr,
+    required this.phoneNumber,
+  });
+}
