da3ea568fea0ba379d827bfe11d66d1b9ccab66b
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-05 11:37:45 +0200

    Edit changed in SizeUtil

--- lib/core/resource/manager_fonts.dart
+++ lib/core/resource/manager_fonts.dart
@@ -1,23 +1,23 @@
 import '/config/all_imports.dart';
 
 class ManagerFontsSizes {
-  static double f7 = SizeUitl.setFontSize(7);
-  static double f8 = SizeUitl.setFontSize(8);
-  static double f9 = SizeUitl.setFontSize(9);
-  static double f10 = SizeUitl.setFontSize(10);
-  static double f11 = SizeUitl.setFontSize(11);
-  static double f12 = SizeUitl.setFontSize(12);
-  static double f13 = SizeUitl.setFontSize(13);
-  static double f14 = SizeUitl.setFontSize(14);
-  static double f15 = SizeUitl.setFontSize(15);
-  static double f16 = SizeUitl.setFontSize(16);
-  static double f17 = SizeUitl.setFontSize(17);
-  static double f18 = SizeUitl.setFontSize(18);
-  static double f20 = SizeUitl.setFontSize(20);
-  static double f22 = SizeUitl.setFontSize(22);
-  static double f25 = SizeUitl.setFontSize(25);
-  static double f34 = SizeUitl.setFontSize(34);
-  static double f32 = SizeUitl.setFontSize(32);
+  static double f7 = SizeUtil.setFontSize(7);
+  static double f8 = SizeUtil.setFontSize(8);
+  static double f9 = SizeUtil.setFontSize(9);
+  static double f10 = SizeUtil.setFontSize(10);
+  static double f11 = SizeUtil.setFontSize(11);
+  static double f12 = SizeUtil.setFontSize(12);
+  static double f13 = SizeUtil.setFontSize(13);
+  static double f14 = SizeUtil.setFontSize(14);
+  static double f15 = SizeUtil.setFontSize(15);
+  static double f16 = SizeUtil.setFontSize(16);
+  static double f17 = SizeUtil.setFontSize(17);
+  static double f18 = SizeUtil.setFontSize(18);
+  static double f20 = SizeUtil.setFontSize(20);
+  static double f22 = SizeUtil.setFontSize(22);
+  static double f25 = SizeUtil.setFontSize(25);
+  static double f34 = SizeUtil.setFontSize(34);
+  static double f32 = SizeUtil.setFontSize(32);
 }
 
 class ManagerFontWeight {
