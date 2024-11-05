f35c8404fa2947e28c808fad3a0347562484f0f8
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-05 11:43:01 +0200

    Create ApiKeys to contain all keys in api

---
+++ lib/config/constants/api_keys.dart
@@ -0,0 +1,36 @@
+class ApiKeys {
+  static const String authorization = 'Authorization';
+  static const String contentType = 'Content-Type';
+  static const String apikey = 'apikey';
+  static const String message = 'message';
+
+  static const String language = 'language';
+  static const String inputLicenseNumber = 'input_license_number';
+  static const String inputJobNumber = 'input_job_number';
+  static const String inputPassword = 'input_password';
+
+  // Driver and Police man Keys
+  static const String id = 'id';
+  static const String image = 'image';
+  static const String firstNameEn = 'first_name_en';
+  static const String firstNameAr = 'first_name_ar';
+  static const String lastNameEn = 'last_name_en';
+  static const String lastNameAr = 'last_name_ar';
+  static const String fatherNameAr = 'father_name_ar';
+  static const String fatherNameEn = 'father_name_en';
+  static const String grandFatherNameAr = 'grand_father_name_ar';
+  static const String grandFatherNameEn = 'grand_father_name_en';
+  static const String phoneNumber = 'phone_number';
+  static const String licenseNumber = 'license_number';
+  static const String idNumber = 'id_number';
+  static const String licenseLevels = 'license_levels';
+  static const String expiryDate = 'expiry_date';
+  static const String releaseDate = 'release_date';
+  static const String numberOfUnReadNotifications =
+      'number_of_un_read_notifications';
+  static const String numberOfViolationsPaid = 'number_of_violations_paid';
+  static const String numberOfViolationsUnPaid = 'number_of_violations_un_paid';
+  static const String militaryRank = 'military_rank';
+  static const String jobNumber = 'job_number';
+
+}
