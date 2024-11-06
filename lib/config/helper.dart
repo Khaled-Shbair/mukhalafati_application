6c963c04dc3dc408f46756a8cd18d981b49b046f
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-06 14:33:38 +0200

    use Fluttertoast Instead of Get.snackbar

--- lib/config/helper.dart
+++ lib/config/helper.dart
@@ -1,10 +1,15 @@
 import 'all_imports.dart';
 
 mixin Helpers {
-  void showSnackBar({required String message}) {
-    Get.snackbar(
-      ManagerStrings.pleaseEnterTheRequiredData,
-      '',
+  void showSnackBar({required String message, bool error = true}) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor:
+          error ? ManagerColors.redNotification : ManagerColors.mayGreen,
+      fontSize: ManagerFontsSizes.f14,
+      gravity: ToastGravity.BOTTOM,
+      textColor: ManagerColors.white,
+      toastLength: Toast.LENGTH_SHORT,
     );
   }
 }
