13187988be206ad86e533316796e3a3b7fb21489
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-07 14:09:17 +0200

    Create DriverChangePasswordResponse to save data response that coming from api in data layer

---
+++ lib/features/change_password/data/response/driver_change_password_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'driver_change_password_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DriverChangePasswordResponse _$DriverChangePasswordResponseFromJson(
+        Map<String, dynamic> json) =>
+    DriverChangePasswordResponse(
+      message: json['message'] as String?,
+    );
+
+Map<String, dynamic> _$DriverChangePasswordResponseToJson(
+        DriverChangePasswordResponse instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+    };
