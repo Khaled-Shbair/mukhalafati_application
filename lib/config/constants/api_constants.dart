cc324c16f716facf35008ddb04ca4b3281df4b42
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-07 13:59:24 +0200

    Add new Url request

--- lib/config/constants/api_constants.dart
+++ lib/config/constants/api_constants.dart
@@ -7,5 +7,14 @@ class ApiConstants {
   static const String apikey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpja252bXNpYWxqeW9meXdtaWFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTQxNjMxNywiZXhwIjoyMDQ0OTkyMzE3fQ.sw_U8RZMKxWBsYNPtTpVf1LH4vbBgYkeEG5KR_Rbq_k';
   static const String driverLogin = '$baseUrl/rest/v1/rpc/driver_login';
-  static const String policeManLogin = '$baseUrl/rest/v1/rpc/police_login';
+  static const String policeManLogin = '$baseUrl/rest/v1/rpc/police_man_login';
+  static const String sendComplaint = '$baseUrl/rest/v1/rpc/send_complaint';
+  static const String forgetPasswordDriver =
+      '$baseUrl/rest/v1/rpc/forget_password_driver';
+  static const String forgetPasswordPoliceMan =
+      '$baseUrl/rest/v1/rpc/forget_password_police_man';
+  static const String changePasswordDriver =
+      '$baseUrl/rest/v1/rpc/driver_change_password';
+  static const String changePasswordPoliceMan =
+      '$baseUrl/rest/v1/rpc/police_man_change_password';
 }
