5f9ffdf26701bb6b59f43da677d0d4c1d70da87e
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-07 14:17:11 +0200

    Change form local database to remote database (api)

--- lib/features/forget_password/presentation/controller/forgot_password_for_driver_controller.dart
+++ lib/features/forget_password/presentation/controller/forgot_password_for_driver_controller.dart
@@ -1,8 +1,8 @@
 import '/config/all_imports.dart';
 
 class ForgotPasswordForDriverController extends GetxController with Helpers {
-  final DriverDatabaseController _driverDatabase =
-      instance<DriverDatabaseController>();
+  final DriverForgotPasswordUseCase _useCase =
+      instance<DriverForgotPasswordUseCase>();
   late TextEditingController licenseNumber;
 
   @override
@@ -24,26 +24,30 @@ class ForgotPasswordForDriverController extends GetxController with Helpers {
 
   void sendButton() async {
     if (_checkDataDriver()) {
-      DriverModel? driver = await _driverDatabase.getDriver(licenseNumber.text);
-      if (driver != null) {
-        String phoneNumber = driver.phone;
-        phoneNumber =
-            // '${number.characters.characterAt(0)}${number.characters.characterAt(1)}${number.characters.characterAt(2)}*****${number.characters.characterAt(8)}${number.characters.characterAt(9)}';
-            '${phoneNumber.characters.characterAt(8)}${phoneNumber.characters.characterAt(9)}*****${phoneNumber.characters.characterAt(0)}${phoneNumber.characters.characterAt(1)}${phoneNumber.characters.characterAt(2)}';
-        Get.toNamed(
-          Routes.verificationCodeScreen,
-          arguments: [
-            phoneNumber,
-            driver.driverId,
-            true,
-            driver.driverVerificationCode,
-          ],
-        );
-      } else {
-        showSnackBar(message: ManagerStrings.licenseNumberNotFound);
-      }
+      (await _useCase.execute(DriverForgotPasswordUseCaseInput(
+              licenseNumber: licenseNumber.text)))
+          .fold(
+        (l) {
+          showSnackBar(message: l.message);
+        },
+        (r) {
+          String phoneNumber = r.phoneNumber!;
+          phoneNumber =
+              // '${number.characters.characterAt(0)}${number.characters.characterAt(1)}${number.characters.characterAt(2)}*****${number.characters.characterAt(8)}${number.characters.characterAt(9)}';
+              '${phoneNumber.characters.characterAt(8)}${phoneNumber.characters.characterAt(9)}*****${phoneNumber.characters.characterAt(0)}${phoneNumber.characters.characterAt(1)}${phoneNumber.characters.characterAt(2)}';
+          Get.toNamed(
+            Routes.verificationCodeScreen,
+            arguments: [
+              phoneNumber,
+              r.driverId,
+              true,
+              0000,
+            ],
+          );
+        },
+      );
     } else {
-      showSnackBar(message: ManagerStrings.pleaseEnterTheRequiredData);
+      showSnackBar(message: ManagerStrings.pleaseEnterLicenseNumber);
     }
   }
 
