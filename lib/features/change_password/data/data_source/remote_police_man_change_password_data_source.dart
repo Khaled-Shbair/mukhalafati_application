c1af997fad76b9e53a51ba1eb1f6b56fa822e477
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-07 14:12:13 +0200

    Create RemotePoliceManChangePasswordDataSource to request data and saved response

---
+++ lib/features/change_password/data/data_source/remote_police_man_change_password_data_source.dart
@@ -0,0 +1,28 @@
+import '/config/all_imports.dart';
+
+abstract class RemotePoliceManChangePasswordDataSource {
+  Future<PoliceManChangePasswordResponse> changePassword(
+      PoliceManChangePasswordRequest request);
+}
+
+class RemotePoliceManChangePasswordDataSourceImpl
+    extends RemotePoliceManChangePasswordDataSource {
+  final AppApi _appApi;
+  final AppSettingsSharedPreferences _sharedPreferences;
+
+  RemotePoliceManChangePasswordDataSourceImpl(
+    this._appApi,
+    this._sharedPreferences,
+  );
+
+  @override
+  Future<PoliceManChangePasswordResponse> changePassword(
+      PoliceManChangePasswordRequest request) async {
+    return await _appApi.changePasswordPoliceMan(
+      request.policeManId,
+      request.newPassword,
+      request.confirmPassword,
+      _sharedPreferences.getLanguage(),
+    );
+  }
+}
