e50a4aa2115002a795d8ec0d241b5a04cf8a8a47
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-13 11:46:02 +0200

    add new api constants

--- lib/config/constants/api_constants.dart
+++ lib/config/constants/api_constants.dart
@@ -8,7 +8,7 @@ class ApiConstants {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpja252bXNpYWxqeW9meXdtaWFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTQxNjMxNywiZXhwIjoyMDQ0OTkyMzE3fQ.sw_U8RZMKxWBsYNPtTpVf1LH4vbBgYkeEG5KR_Rbq_k';
   static const String driverLogin = '$baseUrl/rest/v1/rpc/driver_login';
   static const String policeManLogin = '$baseUrl/rest/v1/rpc/police_man_login';
-  static const String sendComplaint = '$baseUrl/rest/v1/rpc/send_complaint';
+  static const String createComplaint = '$baseUrl/rest/v1/rpc/create_complaint';
   static const String forgetPasswordDriver =
       '$baseUrl/rest/v1/rpc/forget_password_driver';
   static const String forgetPasswordPoliceMan =
@@ -17,4 +17,13 @@ class ApiConstants {
       '$baseUrl/rest/v1/rpc/driver_change_password';
   static const String changePasswordPoliceMan =
       '$baseUrl/rest/v1/rpc/police_man_change_password';
+  static const String getAllComplaintsForDriver =
+      '$baseUrl/rest/v1/rpc/get_all_complaints_for_driver';
+  static const String searchForDriver =
+      '$baseUrl/rest/v1/rpc/search_for_driver';
+  static const String searchOnResultsTestsOfLicense =
+      '$baseUrl/rest/v1/rpc/search_on_results_tests_of_license';
+  static const String createViolation = '$baseUrl/rest/v1/rpc/create_violation';
+  static const String listOfReasonsOfViolations =
+      '$baseUrl/rest/v1/rpc/list_of_reasons_of_violations';
 }
