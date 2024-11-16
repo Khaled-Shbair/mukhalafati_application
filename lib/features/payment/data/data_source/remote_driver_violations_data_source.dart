578687f2c668bac44e42185340806e5076b593ae
Khaled Shbair <khaled.shbair12@gmail.com>
2024-11-16 12:20:21 +0200

    Create request and response and data_source to send request to api and receive response

---
+++ lib/features/payment/data/data_source/remote_driver_violations_data_source.dart
@@ -0,0 +1,19 @@
+import '/config/all_imports.dart';
+
+abstract class RemoteDriverViolationsDataSource {
+  Future<DriverViolationsResponse> getDriverViolations(
+      DriverViolationsRequest request);
+}
+
+class RemoteDriverViolationsDataSourceImpl
+    extends RemoteDriverViolationsDataSource {
+  final AppApi _appApi;
+
+  RemoteDriverViolationsDataSourceImpl(this._appApi);
+
+  @override
+  Future<DriverViolationsResponse> getDriverViolations(
+      DriverViolationsRequest request) async {
+    return await _appApi.getViolationsByDriver(request.driverId);
+  }
+}
---
+++ lib/features/payment/data/data_source/remote_payment_violation_data_source.dart
@@ -0,0 +1,24 @@
+import '/config/all_imports.dart';
+
+abstract class RemotePaymentViolationDataSource {
+  Future<PaymentViolationResponse> paymentViolation(
+      PaymentViolationRequest request);
+}
+
+class RemotePaymentViolationDataSourceImpl
+    extends RemotePaymentViolationDataSource {
+  final AppApi _appApi;
+
+  RemotePaymentViolationDataSourceImpl(this._appApi);
+
+  @override
+  Future<PaymentViolationResponse> paymentViolation(
+      PaymentViolationRequest request) async {
+    return await _appApi.paymentViolation(
+      request.violationId,
+      request.violationState,
+      request.violationPayedBy,
+      request.paymentDateAndTime,
+    );
+  }
+}
