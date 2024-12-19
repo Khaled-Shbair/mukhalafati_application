2f32d2801ea3c26310f4a199349c697a35b7eccb
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-19 13:57:37 +0200

    Control Theme Data and Theme mode from theme_service.dart

--- lib/core/service/theme_service.dart
+++ lib/core/service/theme_service.dart
@@ -1,7 +1,33 @@
 import '/config/all_imports.dart';
 
 class ThemeService {
-  ThemeMode getThemeMode() => ThemeMode.light;
+  ThemeService._();
 
-  ThemeData getThemeData() => managerDarkTheme();
+  void changeTheme(bool isDarkMode) {
+    SharedPreferencesController.setData(
+        SharedPreferencesKeys.darkTheme, isDarkMode);
+    if (isDarkMode) {
+      Get.changeTheme(managerDarkTheme());
+    } else {
+      Get.changeTheme(managerLightTheme());
+    }
+  }
+
+  static ThemeMode get themeMode {
+    if (SharedPreferencesController.getBool(SharedPreferencesKeys.darkTheme)) {
+      return ThemeMode.dark;
+    } else {
+      return ThemeMode.light;
+    }
+  }
+
+  static ThemeData get themeData {
+    if (SharedPreferencesController.getBool(SharedPreferencesKeys.darkTheme)) {
+      return managerDarkTheme();
+    } else {
+      return managerLightTheme();
+    }
+  }
+
+  static ThemeData get darkTheme => managerDarkTheme();
 }
