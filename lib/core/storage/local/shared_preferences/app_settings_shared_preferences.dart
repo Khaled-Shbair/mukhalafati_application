792a3a4c0c741e4738ca6fae10508d8fda449a59
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-19 13:52:43 +0200

    convert from class to file only

--- lib/core/storage/local/shared_preferences/app_settings_shared_preferences.dart
+++ lib/core/storage/local/shared_preferences/app_settings_shared_preferences.dart
@@ -1,105 +1,105 @@
-import 'package:mukhalafati_application/config/all_imports.dart';
+import '/config/all_imports.dart';
 
-class AppSettingsSharedPreferences {
-  // private constructor as I don’t allow creating an instance of this class itself
-  AppSettingsSharedPreferences._();
+Future<void> clearData() async {
+  SharedPreferencesController.removeData(SharedPreferencesKeys.image);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.userId);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.firstName);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.lastName);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.fullNameAr);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.fullNameEn);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.phoneNumber);
+  SharedPreferencesController.removeData(SharedPreferencesKeys.idNumber);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.licenseOrJobNumber);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.rememberMeDriver);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.rememberMePolice);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.expiryDateLicense);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.releaseDateLicense);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.licenseLevelsOfLicense);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.numberOfViolationsUnPaid);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.numberOfViolationsPaid);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.numberOfUnReadNotifications);
+  SharedPreferencesController.removeData(
+      SharedPreferencesKeys.policeMilitaryRank);
+}
 
-  // create only one instance from this class
-  static final AppSettingsSharedPreferences _appSettingsSharedPreferences =
-      AppSettingsSharedPreferences._();
+Future<void> saveDriverData(DriverLoginModel driver) async {
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.numberOfUnReadNotifications,
+      driver.numberOfUnReadNotifications.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.numberOfViolationsPaid,
+      driver.numberOfViolationsPaid.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.numberOfViolationsUnPaid,
+      driver.numberOfViolationsUnPaid.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.userId, driver.id.onNull());
 
-  // create factory constructor to return instance of this class
-  factory AppSettingsSharedPreferences() => _appSettingsSharedPreferences;
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.expiryDateLicense, driver.expiryDate.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.releaseDateLicense, driver.releaseDate.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.idNumber, driver.idNumber.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.licenseLevelsOfLicense,
+      driver.licenseLevels.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.firstName, driver.firstNameAr.onNull());
 
-  static Future<void> clear() async {
-    SharedPreferencesController.removeData(SharedPreferencesKeys.image);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.userId);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.rememberMeDriver);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.rememberMePolice);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.firstName);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.lastName);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.fullNameAr);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.fullNameEn);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.phoneNumber);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.licenseOrJobNumber);
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.lastName, driver.lastNameAr.onNull());
+  await SharedPreferencesController.setData(SharedPreferencesKeys.fullNameAr,
+      '${driver.firstNameAr.onNull()} ${driver.fatherNameAr.onNull()} ${driver.grandFatherNameAr.onNull()} ${driver.lastNameAr.onNull()}');
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.image, driver.image.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.phoneNumber, driver.phone.onNull());
 
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.expiryDateLicense);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.releaseDateLicense);
-    SharedPreferencesController.removeData(SharedPreferencesKeys.idNumber);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.licenseLevelsOfLicense);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.numberOfViolationsUnPaid);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.numberOfViolationsPaid);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.numberOfUnReadNotifications);
-    SharedPreferencesController.removeData(
-        SharedPreferencesKeys.policeMilitaryRank);
-  }
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.licenseOrJobNumber, driver.licenseNumber.onNull());
 
-  static Future<void> setDriverData(DriverLoginModel driver) async {
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.numberOfUnReadNotifications,
-        driver.numberOfUnReadNotifications.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.numberOfViolationsPaid,
-        driver.numberOfViolationsPaid.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.numberOfViolationsUnPaid,
-        driver.numberOfViolationsUnPaid.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.userId, driver.id.onNull());
+  await SharedPreferencesController.setData(SharedPreferencesKeys.fullNameEn,
+      '${driver.firstNameEn.onNull()} ${driver.fatherNameEn.onNull()} ${driver.grandFatherNameEn.onNull()} ${driver.lastNameEn.onNull()}');
+}
 
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.expiryDateLicense, driver.expiryDate.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.releaseDateLicense, driver.releaseDate.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.idNumber, driver.idNumber.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.licenseLevelsOfLicense,
-        driver.licenseLevels.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.firstName, driver.firstNameAr.onNull());
+Future<void> savePoliceData(PoliceManLoginModel police) async {
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.image, police.image.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.phoneNumber, police.phoneNumber.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.firstName, police.firstNameAr.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.lastName, police.lastNameAr.onNull());
 
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.lastName, driver.lastNameAr.onNull());
-    await SharedPreferencesController.setData(SharedPreferencesKeys.fullNameAr,
-        '${driver.firstNameAr.onNull()} ${driver.fatherNameAr.onNull()} ${driver.grandFatherNameAr.onNull()} ${driver.lastNameAr.onNull()}');
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.image, driver.image.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.phoneNumber, driver.phone.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.licenseOrJobNumber, police.jobNumber.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.policeMilitaryRank, police.militaryRank.onNull());
+  await SharedPreferencesController.setData(
+      SharedPreferencesKeys.userId, police.id.onNull());
+}
 
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.licenseOrJobNumber,
-        driver.licenseNumber.onNull());
+Future<void> saveLanguage(String language) async {
+  await SharedPreferencesController.setData(
+    SharedPreferencesKeys.language,
+    language,
+  );
+}
 
-    await SharedPreferencesController.setData(SharedPreferencesKeys.fullNameEn,
-        '${driver.firstNameEn.onNull()} ${driver.fatherNameEn.onNull()} ${driver.grandFatherNameEn.onNull()} ${driver.lastNameEn.onNull()}');
-  }
-
-  static Future<void> setPoliceData(PoliceManLoginModel police) async {
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.image, police.image.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.phoneNumber, police.phoneNumber.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.firstName, police.firstNameAr.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.lastName, police.lastNameAr.onNull());
-
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.licenseOrJobNumber, police.jobNumber.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.policeMilitaryRank, police.militaryRank.onNull());
-    await SharedPreferencesController.setData(
-        SharedPreferencesKeys.userId, police.id.onNull());
-  }
+Future<void> saveOnBoardingViewed() async {
+  await SharedPreferencesController.setData(
+    SharedPreferencesKeys.viewOnBoarding,
+    true,
+  );
 }
