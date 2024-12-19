6c1c4527e3677dccd548396ce51430037c72c3fb
Khaled Shbair <khaled.shbair12@gmail.com>
2024-12-19 13:56:18 +0200

    Edits and enhance on welcome screen (control theme and change image)

--- lib/features/login/presentation/view/screens/welcome_screen.dart
+++ lib/features/login/presentation/view/screens/welcome_screen.dart
@@ -8,72 +8,41 @@ class WelcomeScreen extends StatelessWidget {
     return GetBuilder<WelcomeController>(
       builder: (controller) {
         return Scaffold(
-          backgroundColor: ManagerColors.white,
-          body: Stack(
+          resizeToAvoidBottomInset: false,
+          body: Column(
             children: [
               Image.asset(
                 ManagerAssets.backgroundWelcomeScreen,
+                width: ManagerWidth.infinity,
+                height: ManagerHeight.h406,
                 fit: BoxFit.cover,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsetsDirectional.only(
-                      start: ManagerWidth.w38,
-                      end: ManagerWidth.w20,
-                      bottom: ManagerHeight.h20,
-                    ),
-                    child: SvgPicture.asset(
-                      ManagerAssets.welcomeImage,
-                      width: ManagerWidth.w268,
-                      fit: BoxFit.cover,
-                    ),
+              verticalSpace(ManagerHeight.h40),
+              Text(
+                ManagerStrings.welcome,
+                textAlign: TextAlign.center,
+                style: context.textTheme.titleLarge,
+              ),
+              verticalSpace(ManagerHeight.h10),
+              Text(
+                ManagerStrings.subTitleWelcome,
+                textAlign: TextAlign.center,
+                style: context.textTheme.bodyMedium,
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.only(
+                  start: ManagerWidth.w28,
+                  end: ManagerWidth.w28,
+                  bottom: ManagerHeight.h16,
+                  top: ManagerHeight.h30,
+                ),
+                child: mainButton(
+                  onPressed: () => controller.loginButton(context),
+                  child: Text(
+                    ManagerStrings.login,
+                    style: context.textTheme.labelMedium,
                   ),
-                  Text(
-                    ManagerStrings.welcome,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: ManagerFontFamily.cairo,
-                      color: ManagerColors.blackOlive,
-                      fontSize: ManagerFontsSizes.f22,
-                      fontWeight: ManagerFontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: ManagerHeight.h10),
-                  Text(
-                    ManagerStrings.subTitleWelcome,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: ManagerFontFamily.cairo,
-                      color: ManagerColors.blackOlive,
-                      fontSize: ManagerFontsSizes.f14,
-                      fontWeight: ManagerFontWeight.medium,
-                      height: AppConstants.heightOfSubTitleWelcomeScreen,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsetsDirectional.only(
-                      start: ManagerWidth.w28,
-                      end: ManagerWidth.w28,
-                      bottom: ManagerHeight.h16,
-                      top: ManagerHeight.h30,
-                    ),
-                    child: mainButton(
-                      onPressed: () => controller.loginButton(),
-                      child: Text(
-                        ManagerStrings.login,
-                        style: TextStyle(
-                          color: ManagerColors.white,
-                          fontFamily: ManagerFontFamily.cairo,
-                          fontWeight: ManagerFontWeight.bold,
-                          fontSize: ManagerFontsSizes.f16,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ],
           ),
